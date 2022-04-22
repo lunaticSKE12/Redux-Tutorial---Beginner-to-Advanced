bd571d56398f87cd98e729f6dff621d754c4c46a
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-04-22 12:31:03 +0700

    Sending asynchronous HTTP Requests with Redux and Firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import './App.css';
 import Auth from './components/Auth';
 import Layout from './components/Layout';
 
 function App() {
+  const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  console.log(isLoggedIn);
   const cartItems = useSelector((state) => state.cart.itemsList);
-  console.log(cartItems);
+  useEffect(() => {
+    const sendRequest = async () => {
+      const res = await fetch(
+        'https://redux-http-64149-default-rtdb.asia-southeast1.firebasedatabase.app/cartItems.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+    };
+    sendRequest();
+  }, [cart]);
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
