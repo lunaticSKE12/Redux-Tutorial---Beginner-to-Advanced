43b2f31ae3a5d1f16f439b27deabb40b3ac0adc2
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-04-22 13:36:53 +0700

    Show notification

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import './App.css';
 import Auth from './components/Auth';
 import Layout from './components/Layout';
 import Notification from './components/Notification';
-
+import { uiActions } from './store/ui-slice';
+let isFirstRender = true;
 function App() {
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
   const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const cartItems = useSelector((state) => state.cart.itemsList);
+
   useEffect(() => {
+    if (isFirstRender) {
+      isFirstRender = false;
+      return;
+    }
     const sendRequest = async () => {
+      // Send state as Sending request
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: 'Sending Request',
+          type: 'warning',
+        })
+      );
       const res = await fetch(
         'https://redux-http-64149-default-rtdb.asia-southeast1.firebasedatabase.app/cartItems.json',
         {
@@ -20,12 +36,30 @@ function App() {
         }
       );
       const data = await res.json();
+      // Send state as Request is successful
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: 'Sent Request to database Successfully',
+          type: 'success',
+        })
+      );
     };
-    sendRequest();
+    sendRequest().catch((err) => {
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: 'Sending Request Failed',
+          type: 'error',
+        })
+      );
+    });
   }, [cart]);
   return (
     <div className="App">
-      <Notification type="success" message={'this is test msg'} />
+      {notification && (
+        <Notification type={notification.type} message={notification.message} />
+      )}
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
     </div>
--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Alert } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../store/ui-slice';
 
 const Notification = ({ type, message }) => {
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
+  const handleClose = () => {
+    dispatch(
+      uiActions.showNotification({
+        open: false,
+      })
+    );
+  };
   return (
     <div>
-      <Alert severity={type}>{message}</Alert>
+      {notification.open && (
+        <Alert onClose={handleClose} severity={type}>
+          {message}
+        </Alert>
+      )}
     </div>
   );
 };
