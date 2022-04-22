860b41cf5eb411a50e99c81717812fb778b02be7
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-04-22 12:17:55 +0700

    Add, remove item from cart, update total price and items in cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -28,7 +28,17 @@ const cartSlice = createSlice({
         state.totalQuantity++;
       }
     },
-    removeFromCart() {},
+    removeFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.itemsList.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.itemsList = state.itemsList.filter((item) => item.id !== id);
+        state.totalQuantity--;
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
+    },
     setShowCart(state) {
       state.showCart = !state.showCart;
     },
